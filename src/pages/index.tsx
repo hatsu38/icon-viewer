c53de76fee3200d078b6489e79c15a0828b840d5
hatsu38 <hajiwata0308@gmail.com>
2024-05-31 16:10:42 +0900

    feat: Add Light/Dark Mode Icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import D from "@/assets/icons/d.png";
 import E from "@/assets/icons/e.png";
 import { SnsWrapper } from "../components/snsWrapper";
 import { FileUploader } from "../components/fileUploader";
+import { Icon } from "@/components/icon";
 
 export default function Home() {
   const [imageSrc, setImageSrc] = useState<string>("");
@@ -31,15 +32,16 @@ export default function Home() {
   return (
     <div className={`${isDarkMode ? 'dark' : ''}`}>
       <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} bg-white text-black dark:bg-gray-900 dark:text-white`}
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} bg-white text-gray-700 dark:bg-gray-900 dark:text-white`}
       >
         <div className="flex flex-col items-center w-full">
           <button
             onClick={toggleDarkMode}
-            className="mb-4 p-2 bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded"
+            className={`fixed top-4 right-4 p-2rounded-md ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-700'} dark:bg-gray-800 dark:text-white`}
           >
-            Toggle Dark Mode
+            <Icon icon={isDarkMode ? "mdDarkMode" : "mdOutlineLightMode"} size="1.5rem" />
           </button>
+          <h1 className="text-3xl font-bold mb-4">SNS Icon Viewer</h1>
           <FileUploader imageSrc={imageSrc} onFileSrcChange={(src) => setImageSrc(src as string)} className="mb-4" />
           {imageSrc && (
             <div className="flex items-start space-x-8">
